1496f773448f735c01ec20040ab31dc771336051
hengheng <240668871@qq.com>
2016-05-27 16:33:36 +0800

    配置XMPP环境

---
+++ WC/WC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WC
+//
+//  Created by YuanMiaoHeng on 16/5/27.
+//  Copyright © 2016年 Mi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WC/WCUITests/WCUITests.m
@@ -0,0 +1,40 @@
+//
+//  WCUITests.m
+//  WCUITests
+//
+//  Created by YuanMiaoHeng on 16/5/27.
+//  Copyright © 2016年 Mi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WCUITests : XCTestCase
+
+@end
+
+@implementation WCUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
